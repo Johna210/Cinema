21b5c89edcec3b9692708773d7c498fc1042e75e
Johna210 <solomonjohna21@gmail.com>
2024-01-05 13:47:36 +0300

    Added backend modules,controllers and services

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CinemasModule } from './cinemas/cinemas.module';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, CinemasModule, MoviesModule],
   controllers: [AppController],
   providers: [AppService],
 })
