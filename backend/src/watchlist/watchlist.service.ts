48b9acd7457796dc27420ad32883df58ba4e0b4e
dawit belay <dawitbelay0707@gmail.com>
2024-01-09 23:11:02 +0300

    constructor add in watchListService

--- backend/src/watchlist/watchlist.service.ts
+++ backend/src/watchlist/watchlist.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { WatchList } from './watchlist.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class WatchlistService {}
+export class WatchlistService {
+    constructor(@InjectRepository(WatchList) private watchListRepository: Repository<WatchList>){}
+}
