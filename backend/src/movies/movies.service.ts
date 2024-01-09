b926d1a39385f52744a0c8617c7ac68ab5d422e5
dawit belay <dawitbelay0707@gmail.com>
2024-01-09 22:41:12 +0300

    create movie created in moviesService

--- backend/src/movies/movies.service.ts
+++ backend/src/movies/movies.service.ts
@@ -7,4 +7,18 @@ import { Repository } from 'typeorm';
 export class MoviesService {
     constructor(@InjectRepository(Movies) private moviesRepository: Repository<Movies>){}
     
+
+    //for Creating movies
+    async createMovies(title:string,genre:string,day:string,showTime:string,imageUrl:string,cinemaId:number){
+        const newmovie = this.moviesRepository.create({
+            title,
+            genre,
+            day,
+            showTime,
+            imageUrl,
+            cinemaId})
+            
+        return this.moviesRepository.save(newmovie)
+       }
+
 }
