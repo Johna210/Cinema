b967b5e6e35cb0536da19d2c80b885b3d8e8085c
dawit belay <dawitbelay0707@gmail.com>
2024-01-09 10:19:36 +0300

    unwanted comment removed

--- backend/src/movies/movies.entity.ts
+++ backend/src/movies/movies.entity.ts
@@ -25,29 +25,3 @@ export class Movies{
 }
 
 
-// import { Column, PrimaryGeneratedColumn } from "typeorm";
-
-
-
-// export class Movies{
-//     @PrimaryGeneratedColumn()
-//     id:number;
-
-//     @Column()
-//     title:string;
-
-//     @Column()
-//     genre:string;
-
-//     @Column()
-//     day: string;
-
-//     @Column()
-//     showTime:string;
-
-//     @Column()
-//     imageUrl:string;
-
-//     @Column()
-//     creatorName:string;
-// }
