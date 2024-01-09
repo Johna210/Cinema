3ea05da94b80b595c40e5ca8b776880230b2ad9f
dawit belay <dawitbelay0707@gmail.com>
2024-01-09 22:23:30 +0300

    constructer added in the moviesService

--- backend/src/movies/movies.service.ts
+++ backend/src/movies/movies.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Movies } from './movies.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+    constructor(@InjectRepository(Movies) private moviesRepository: Repository<Movies>){}
+    
+}
