694024efb2c17aa0c94fd1f37bfa32a080d237df
dawit belay <dawitbelay0707@gmail.com>
2024-01-09 23:14:19 +0300

    create watchlist added to watchlistService

--- backend/src/watchlist/watchlist.service.ts
+++ backend/src/watchlist/watchlist.service.ts
@@ -6,4 +6,13 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class WatchlistService {
     constructor(@InjectRepository(WatchList) private watchListRepository: Repository<WatchList>){}
+
+    // To create watchlist using the userId and movieId as parameter 
+    createWatchList(userId:number,movieId:number){
+
+        const watchList = this.watchListRepository.create({userId,movieId})
+
+        return this.watchListRepository.save(watchList);
+
+    }
 }
