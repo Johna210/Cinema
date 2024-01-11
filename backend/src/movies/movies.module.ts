59cb068a42a230568c2f3284b1c89e762a8d4ae6
Johna210 <solomonjohna21@gmail.com>
2024-01-11 10:58:15 +0300

    Added images with upload to the server

--- backend/src/movies/movies.module.ts
+++ backend/src/movies/movies.module.ts
@@ -3,7 +3,6 @@ import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Movies } from './movies.entity';
-import { CinemasService } from 'src/cinemas/cinemas.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Movies])],
